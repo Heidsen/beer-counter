8be73eb8cd1382836f84fe59e4a4142ff08f61ae
start.vaadin.com <no-reply@vaadin.com>
2022-08-05 09:37:17 +0000

    Generated project

---
+++ src/main/java/com/heidsen/views/counter/CounterView.java
@@ -0,0 +1,35 @@
+package com.heidsen.views.counter;
+
+import com.heidsen.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Counter")
+@Route(value = "start", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class CounterView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public CounterView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
