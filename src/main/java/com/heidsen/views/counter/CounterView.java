e05e8c2190d1551019c1340c0021d9f4897994c8
heidsen <heidsen@gmx.de>
2022-08-05 14:02:52 +0200

    leggo

--- src/main/java/com/heidsen/views/counter/CounterView.java
+++ src/main/java/com/heidsen/views/counter/CounterView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -15,21 +16,46 @@ import com.vaadin.flow.router.RouteAlias;
 @RouteAlias(value = "", layout = MainLayout.class)
 public class CounterView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private NumberField beerAmount;
+
+    private Button oneFifthLiter;
+    private Button oneThirdLiter;
+    private Button halfLiter;
+
+    private Button resetBeer;
 
     public CounterView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+
+        beerAmount = new NumberField("amount of beer");
+        beerAmount.setValue(0.0);
+        beerAmount.setEnabled(false);
+        oneFifthLiter = new Button("0,2l");
+        oneFifthLiter.addClickListener(e -> {
+            Notification.show("added 0,2l ");
+            beerAmount.setValue(getCurrentBeerAmount() + 0.2);
         });
-        sayHello.addClickShortcut(Key.ENTER);
+        oneThirdLiter = new Button("0,33l");
+        oneThirdLiter.addClickListener(e -> {
+            Notification.show("added 0,33l ");
+            beerAmount.setValue(getCurrentBeerAmount() + 0.33);
+        });
+        halfLiter = new Button("0,5l");
+        halfLiter.addClickListener(e -> {
+            Notification.show("added 0,5l ");
+            beerAmount.setValue(getCurrentBeerAmount() + 0.5);
+        });
+        resetBeer = new Button("Reset");
+        resetBeer.addClickListener(e -> {
+            beerAmount.setValue(0.0);
+        });
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        setVerticalComponentAlignment(Alignment.END, beerAmount, oneFifthLiter, oneThirdLiter, halfLiter, resetBeer);
 
-        add(name, sayHello);
+        add(beerAmount, oneFifthLiter, oneThirdLiter, halfLiter, resetBeer);
     }
 
+    private Double getCurrentBeerAmount() {
+        return beerAmount.getValue() == null ? 0.0 : beerAmount.getValue();
+    }
 }
